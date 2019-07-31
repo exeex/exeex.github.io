7588a39d1d3ed4ebacfa996cc3fd33c5b53b7ece
cswu <xray0h@gmail.com>
2019-07-31 15:23:58 +0800

    f

--- d.py
+++ d.py
@@ -5,26 +5,33 @@ import time
 # path to it (e.g. '/tmp/tmpb48zma.txt') in the `file_name` variable:
 
 
-url = 'https://d1q8jl7i9mxhvr.cloudfront.net/AUDIO/REF/test/{}/vocals.m4a'
+url_pattern = 'https://d1q8jl7i9mxhvr.cloudfront.net/AUDIO/REF/test/{}/vocals.m4a'
 
 with open('list.csv', 'r') as f:
-    for line in f.readlines():
-        name = line.strip()
-        url = url.replace('https://', '')
-        url = urllib.parse.quote(url.format(name))
-        url = 'https://' + url
-        print(url)
-        opener = urllib.request.build_opener()
-        opener.addheaders = [('User-agent',
-                              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36")]
-        urllib.request.install_opener(opener)
+    for idx, line in enumerate(f.readlines()):
 
-        file_name = f'vocals-{name}.m4a'
+        if idx > 16:
+            name = line.strip()
+            url = url_pattern.replace('https://', '')
+            _name = name.replace(' ', '%20')
+            _name = _name.replace('amp;', '')
+            url = url.format(_name)
+            url = 'https://' + url
+            print(url)
+            opener = urllib.request.build_opener()
+            opener.addheaders = [('User-agent',
+                                  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36")]
+            urllib.request.install_opener(opener)
 
-        with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
-            data = response.read()  # a `bytes` object
-            out_file.write(data)
+            file_name = f'vocals-{name}.m4a'
 
-        print(file_name)
+            with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+                data = response.read()  # a `bytes` object
+                out_file.write(data)
 
-        time.sleep(5)
+            print(file_name)
+
+            time.sleep(5)
+
+
+
