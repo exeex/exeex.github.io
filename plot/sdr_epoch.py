61b703d8dce3ede44f6b86b0b8b9f7e67281262a
cswu <xray0h@gmail.com>
2019-08-09 20:52:32 +0800

    add plot epochs

---
+++ plot/sdr_epoch.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import os
+import csv
+from pathlib import Path
+
+data_folder = Path('data')
+files = os.listdir(data_folder)
+
+for file in files:
+    csv_file = data_folder / file
+
+    with open(csv_file, 'r') as f:
+        r = csv.reader(f)
+        next(r)
+        a = [(time, int(epoch), float(sdr)) for time, epoch, sdr in r]
+        times, epochs, sdrs = zip(*a)
+        print(epochs)
+        print(sdrs)
+        plt.plot(list(epochs), list(sdrs), label=file.split('.')[0])
+    # break
+
+plt.xlabel("epoch")
+plt.ylabel("sdr(dB)")
+plt.title("PyPlot First Example")
+plt.ylim(-2.5, 5)
+plt.legend(loc='lower right')
+
+plt.show()
